aa5d3ff49e39eadeba5d15404c4c6c09b3549250
Omar Brikaa <brikaaomar@gmail.com>
2023-04-21 21:39:17 +0200

    Some constructor tests

--- JFreeChart/src/test/java/org/jfree/data/test/QuarterClassTest.java
+++ JFreeChart/src/test/java/org/jfree/data/test/QuarterClassTest.java
@@ -1,25 +1,78 @@
 package org.jfree.data.test;
 
 import org.junit.Assert;
+
+import java.util.Date;
+import java.util.TimeZone;
+
 import org.jfree.data.time.Quarter;
+import org.jfree.data.time.Year;
 import org.junit.Test;
 
 public class QuarterClassTest {
-    Quarter quarter;
-
-    private void arrange(Integer quart, Integer year) {
-        quarter = new Quarter(quart, year);
+    private void assertQuarter(Quarter quarter, int year, int quarterInt) {
+        Assert.assertEquals(year, quarter.getYear().getYear());
+        Assert.assertEquals(quarterInt, quarter.getQuarter());
     }
 
-    private void arrange() {
-        quarter = new Quarter();
+
+    @Test
+    public void testDefaultConstructor() {
+        Quarter quarter = new Quarter();
+        assertQuarter(quarter, 2023, 2);
     }
 
     @Test
-    public void testQuarterDefaultConstructor() {
-        arrange();
-        Assert.assertEquals(2023, quarter.getYear().getYear());
-        Assert.assertEquals(2, quarter.getQuarter());
+    public void testQuarterAndIntYearConstructor() {
+        Quarter quarter = new Quarter(3, 2023);
+        assertQuarter(quarter, 2023, 3);
     }
 
+    @Test(expected=IllegalArgumentException.class) // Spec says supported range is from Q1 to Q4
+    public void testNegativeQuarterConstructor() {
+        new Quarter(-1, 2023);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testOverLimitQuarterConstructor() {
+        new Quarter(5, 2023);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testNegativeYearConstructor() {
+        new Quarter(1, -2023);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testOverLimitIntYearConstructor() {
+        new Quarter(3, 10000);
+    }
+
+    @Test
+    public void testQuarterAndActualYearConstructor() {
+        Year year = new Year(2023);
+        Quarter quarter = new Quarter(3, year);
+        assertQuarter(quarter, 2023, 3);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testOverLimitActualYearConstructor() {
+        Year year = new Year(10000);
+        new Quarter(3, year);
+    }
+
+    @Test
+    public void testDateConstructor() {
+        Date date = new Date(1672531200000l);
+        Quarter quarter = new Quarter(date);
+        assertQuarter(quarter, 2023, 1);
+    }
+
+    @Test
+    public void testDateAndTimezoneConstructor() {
+        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+        Date date = new Date(1672531200000l);
+        Quarter quarter = new Quarter(date, tz);
+        assertQuarter(quarter, 2023, 1);
+    }
 }
